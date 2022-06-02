2496b309658a31a1d72d29b8c202ddda386bc49c
ganeodolu <ganeodolu@gmail.com>
2022-06-03 01:32:22 +0900

    feat: 스타일 추가

---
+++ image-community/src/components/Post.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Grid from "../elements/Grid";
+
+const Post = (props) => {
+  return (
+		<div>
+			<Grid padding="16px">
+				<div>user profile/ user name / insert_dt / is_me (edit button)</div>
+				<div>contents</div>
+				<div>image</div>
+				<div>comment cnt</div>
+			</Grid>
+		</div>
+	);
+}
+
+// 기본 props 지정해서 비어있을 때 오류 방지
+Post.defaultProps = {
+	user_info: {
+		user_name: "mean0",
+		user_profile:
+			"http://www.chemicalnews.co.kr/news/photo/202106/3636_10174_4958.jpg",
+	},
+	image_url:
+    "http://www.chemicalnews.co.kr/news/photo/202106/3636_10174_4958.jpg",
+  contents: "괭이",
+  comment_cnt: 10,
+  insert_dt: "2022-06-02 18:00:00",
+};
+
+export default Post
--- image-community/src/pages/PostList.js
+++ image-community/src/pages/PostList.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Post from '../components/Post'
 
-const PostList = () => {
+const PostList = (props) => {
   return (
     <React.Fragment>
-      목록 페이지
+      <Post />
     </React.Fragment>
   )
 }
