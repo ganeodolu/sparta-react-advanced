06758807aa5665ff557f8b7233da1280122d864b
ganeodolu <ganeodolu@gmail.com>
2022-06-04 21:09:08 +0900

    style: input 컴포넌트 margin 추가

--- image-community/src/elements/Input.js
+++ image-community/src/elements/Input.js
@@ -1,23 +1,24 @@
 import React from 'react'
+import styled from "styled-components";
 
 const Input = (props) => {
-  const { children, placeholderText } = props;
-
+  const { marginBottom, children, placeholderText } = props;
+  const styles = { marginBottom };
   return (
-		<>
+		<InputBox {...styles}>
       <label>{children}</label>
       <div />
 			<input type="text" placeholder={placeholderText + " 입력하세요"}></input>
-		</>
+		</InputBox>
 	);
 }
 
 Input.defaultProps = {
-
+  marginBottom: '20px',
 }
 
-const InputBox = {
-
-}
+const InputBox = styled.div`
+  margin-bottom: ${({ marginBottom }) => marginBottom};
+`
 
 export default Input
