8f764b8855b6f8d319c6318032f37d8b07382d8c
ganeodolu <ganeodolu@gmail.com>
2022-06-05 20:19:58 +0900

    refactor: 클릭이벤트함수 헤더로 이동

--- image-community/src/elements/Button.js
+++ image-community/src/elements/Button.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
 
 const Button = (props) => {
-  const navigate = useNavigate();
-  const { width, height, backgroundColor, border, address, children } = props;
-  
-  const onClickButton = (str) => {
-    navigate(`/${str}`)
-  }
+  const { onClickButton, width, height, backgroundColor, border, disabled, children } = props; 
   const styles = { width, height, backgroundColor, border }
 
   return (
-    <ButtonBox {...styles} type="button" onClick={() => onClickButton(address)}>
+    <ButtonBox {...styles} disabled={disabled? true : null} type="button" onClick={() => onClickButton()}>
 			{children}
 		</ButtonBox>
 	);
@@ -23,13 +17,14 @@ Button.defaultProps = {
 	height: "40px",
   backgroundColor: "#C4C4C4",
   border: 0,
+  disabled: false,
 };
 
 const ButtonBox = styled.button`
   background-color: ${({backgroundColor}) => backgroundColor} ;
   width: ${(props) => props.width};
   height: ${({ height }) => height};
-  border: ${({border}) => border};
+  border: ${({ border }) => border};
 `
 
 export default Button
--- image-community/src/shared/Header.js
+++ image-community/src/shared/Header.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Grid, Button } from '../elements';
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const onClickButton = (str) => {
+		navigate(`/${str}`);
+	};
 
-
-  // Button onClick 실행함수를 Header에 직접 넣는 방법은??
   return (
 		<Grid is_flex padding="8px">
-			<Button address="">홈</Button>
-			<Button address="login">로그인</Button>
-			<Button address="signup">회원가입</Button>
+			<Button onClickButton={()=>onClickButton('')}>
+				홈
+			</Button>
+			<Button onClickButton={() => onClickButton('login')}>
+				로그인
+			</Button>
+			<Button onClickButton={()=> onClickButton('signup')}>
+				회원가입
+			</Button>
 		</Grid>
 	);
 }
