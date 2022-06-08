b32eb6fc6aa6f7b0cf6e36925910bf65373ae362
ganeodolu <ganeodolu@gmail.com>
2022-06-08 17:26:11 +0900

    feat: 쿠키 핸들러 추가

--- image-community/src/pages/Login.js
+++ image-community/src/pages/Login.js
@@ -1,19 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input, Button, Text } from "../elements";
+import Cookie from "../shared/Cookie";
 
 const Login = (props) => {
-	const onClickButton = (userId, userPassword) => {};
+	const [state, setState] = useState({
+		id: "",
+		password: "",
+	})
+	const onChangeInput = (e) => {
+		setState({
+			...state,
+			[e.target.name]: e.target.value,
+		});
+	};
+	const onClickButton = (userId, userPassword) => {
+		Cookie.set('userId', userId, 3)
+		Cookie.set('userPwd', userPassword, 3)
+		Cookie.get('userId')
+		Cookie.del('userPwd')
+	};
 
 	return (
 		<div style={{ padding: "16px" }}>
 			<Text size="32px">로그인</Text>
-			<Input placeholderText="아이디를">아이디</Input>
+			<Input
+				name="id"
+				placeholderText="아이디를"
+				value={state.id}
+				onChange={onChangeInput}
+			>
+				아이디
+			</Input>
 			<div />
-			<Input type="password" placeholderText="비밀번호를">
+			<Input
+				name="password"
+				type="password"
+				placeholderText="비밀번호를"
+				value={state.password}
+				onChange={onChangeInput}
+			>
 				비밀번호
 			</Input>
 			<div />
-			<Button width="80vw" onClickButton={onClickButton}>
+			<Button width="80vw" onClickButton={() => onClickButton(state.id, state.password)}>
 				로그인하기
 			</Button>
 		</div>
---
+++ image-community/src/shared/Cookie.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const makeExpiredDate = (day) => {  
+  let date = new Date();
+  date.setDate(date.getDate() + day);
+  return date.toUTCString();
+}
+
+const Cookie = {
+  set: (name, value, day = 5) => {
+    document.cookie = `${name}=${value}; expires=${makeExpiredDate(day)}`;
+  },
+  get: (name) => {
+    let value = `; ${document.cookie}`;
+    let parts = value.split(`; ${name}=`);
+    if (parts.length === 2) {
+      return parts[1].split(';')[0]
+    }
+    console.log('해당 쿠키가 없습니다')
+  },
+  del: (name) => {
+    document.cookie = `${name}=; expires=${makeExpiredDate(-1)}` 
+  }
+}
+
+export default Cookie
