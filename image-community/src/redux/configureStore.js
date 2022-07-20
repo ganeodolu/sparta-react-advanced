12e0cd6037e3e6a7f908184bd2a2ef339d403094
ganeodolu <ganeodolu@gmail.com>
2022-07-20 23:47:02 +0900

    etc: createStore 삭제

--- image-community/src/redux/configureStore.js
+++ image-community/src/redux/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { createBrowserHistory } from "history";
 import { connectRouter } from "connected-react-router";
